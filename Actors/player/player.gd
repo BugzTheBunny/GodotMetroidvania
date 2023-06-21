6645400e3f8b0cfc33b21c70ada11bc02e4a200b
Slava <slaval@eternity-it.co.il>
2023-06-21 21:50:35 +0300

    added some basic movement code

--- Actors/player/player.gd
+++ Actors/player/player.gd
@@ -2,14 +2,28 @@ extends CharacterBody2D
 
 @export var speed = 200.0
 
-@onready var movement_vector : Vector2 = Vector2.ZERO 
+@onready var movement_axis : float = 0
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var anim_player : AnimationPlayer = $AnimationPlayer
 
 func _process(delta):
 	handle_movement(delta)
+	handle_sprite()
+	move_and_slide()
+	
 
 func handle_movement(delta):
-	movement_vector = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	if movement_vector.x:
-		print("all good!")
+	movement_axis = Input.get_axis("ui_left","ui_right")
+	if movement_axis:
+		velocity.x = speed * sign(movement_axis)
+		anim_player.play("Walk")
 	else:
-		print("not moving")
+		velocity.x = move_toward(velocity.x, 0, speed)
+		anim_player.play("Idle")
+
+func handle_sprite():
+	if movement_axis > 0 :
+		sprite.flip_h = false
+	elif movement_axis < 0 :
+		sprite.flip_h = true
+			
