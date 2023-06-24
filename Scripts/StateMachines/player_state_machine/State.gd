be70c2f7e9b39484f08c1759511ac7f9e440a78d
slava <urigami2010@gmail.com>
2023-06-24 23:24:55 +0300

    adding animation tree

--- Scripts/StateMachines/player_state_machine/State.gd
+++ Scripts/StateMachines/player_state_machine/State.gd
@@ -4,6 +4,9 @@ class_name State
 @export var can_move : bool = true
 
 func _physics_process(delta):
+	_to_physic_proccess(delta)
+	
+func _to_physic_proccess(delta):
 	pass
 
 func _unhandled_input(event):
--- Scripts/StateMachines/player_state_machine/States/ground.gd
+++ Scripts/StateMachines/player_state_machine/States/ground.gd
@@ -2,13 +2,19 @@ extends State
 class_name GroundState
 
 var parent : PlayerStateMachine
+var player : CharacterBody2D
 
 func _ready():
 	parent = get_parent()
+	player = parent.get_parent()
 	parent.transitioned.connect(enter.bind(self))
+
+	
+func _to_physic_proccess(delta):
+	if player.is_on_floor():
+		parent.animation_player.play("Idle")
 	
 func enter():
-	parent.animation_player.play("Idle")
-
+	pass
 func exit():
 	pass
